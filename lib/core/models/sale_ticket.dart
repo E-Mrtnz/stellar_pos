508419df2f11c33094f6da425bac24eb543146c0
E-Z <68935117+E-Mrtnz@users.noreply.github.com>
2026-09-03 22:27:13 -0600

    feat: add sale ticket data model

---
+++ lib/core/models/sale_ticket.dart
@@ -0,0 +1,43 @@
+class SaleTicketItem {
+  final int quantity;
+  final String description;
+  final double unitPrice;
+  final double discount;
+  final double total;
+
+  const SaleTicketItem({
+    required this.quantity,
+    required this.description,
+    required this.unitPrice,
+    required this.discount,
+    required this.total,
+  });
+}
+
+class SaleTicketData {
+  final String ticketNumber;
+  final String date;
+  final String time;
+  final String client;
+  final List<SaleTicketItem> items;
+  final double subtotal;
+  final double discount;
+  final double total;
+  final String paymentMethod;
+  final double received;
+  final double change;
+
+  const SaleTicketData({
+    required this.ticketNumber,
+    required this.date,
+    required this.time,
+    required this.client,
+    required this.items,
+    required this.subtotal,
+    required this.discount,
+    required this.total,
+    required this.paymentMethod,
+    required this.received,
+    required this.change,
+  });
+}
