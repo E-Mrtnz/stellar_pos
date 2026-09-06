11d0eae7321331ce76cf40f6bb761d89d4e51141
E-Z <68935117+E-Mrtnz@users.noreply.github.com>
2026-09-05 19:23:40 -0600

    feat: add client address

--- lib/core/models/client.dart
+++ lib/core/models/client.dart
@@ -2,23 +2,36 @@ class Client {
   final String id;
   final String name;
   final String phone;
+  final String address;
 
   const Client({
     required this.id,
     required this.name,
     required this.phone,
+    this.address = '',
   });
 
-  Client copyWith({String? id, String? name, String? phone}) {
+  Client copyWith({
+    String? id,
+    String? name,
+    String? phone,
+    String? address,
+  }) {
     return Client(
       id: id ?? this.id,
       name: name ?? this.name,
       phone: phone ?? this.phone,
+      address: address ?? this.address,
     );
   }
 
   Map<String, dynamic> toMap() {
-    return {'id': id, 'name': name, 'phone': phone};
+    return {
+      'id': id,
+      'name': name,
+      'phone': phone,
+      'address': address,
+    };
   }
 
   factory Client.fromMap(Map<String, dynamic> map) {
@@ -26,6 +39,7 @@ class Client {
       id: map['id']?.toString() ?? '',
       name: map['name']?.toString() ?? '',
       phone: map['phone']?.toString() ?? '',
+      address: map['address']?.toString() ?? '',
     );
   }
 }
