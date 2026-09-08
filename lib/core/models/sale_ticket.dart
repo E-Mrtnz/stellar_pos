0a5c4b960d292b11e0ae2234b3919d26ff53f3ae
E-Z <68935117+E-Mrtnz@users.noreply.github.com>
2026-09-08 15:12:48 -0600

    feat: include brand in ticket items

--- lib/core/models/sale_ticket.dart
+++ lib/core/models/sale_ticket.dart
@@ -1,17 +1,12 @@
 class SaleTicketItem {
   final int quantity;
   final String description;
+  final String brand;
   final double unitPrice;
   final double discount;
   final double total;
 
-  const SaleTicketItem({
-    required this.quantity,
-    required this.description,
-    required this.unitPrice,
-    required this.discount,
-    required this.total,
-  });
+  const SaleTicketItem({required this.quantity, required this.description, this.brand = '', required this.unitPrice, required this.discount, required this.total});
 }
 
 class SaleTicketData {
@@ -28,18 +23,5 @@ class SaleTicketData {
   final double received;
   final double change;
 
-  const SaleTicketData({
-    required this.ticketNumber,
-    required this.date,
-    required this.time,
-    required this.client,
-    required this.items,
-    required this.subtotal,
-    required this.discount,
-    this.cardFee = 0,
-    required this.total,
-    required this.paymentMethod,
-    required this.received,
-    required this.change,
-  });
+  const SaleTicketData({required this.ticketNumber, required this.date, required this.time, required this.client, required this.items, required this.subtotal, required this.discount, this.cardFee = 0, required this.total, required this.paymentMethod, required this.received, required this.change});
 }
