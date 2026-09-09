c389d185de7c09a0c2f8a869b69dded660c85edd
E-Z <68935117+E-Mrtnz@users.noreply.github.com>
2026-09-09 00:53:57 -0600

    feat(storage): initialize local storage before app startup

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 
 import 'package:stellar_pos/app.dart';
+import 'package:stellar_pos/core/data/storage/local_storage.dart';
+import 'package:stellar_pos/core/data/storage/storage_schema.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await LocalStorage.initialize();
+  await StorageSchema.initialize();
+
   runApp(
     const AppProviders(
       child: StellarPosApp(),
