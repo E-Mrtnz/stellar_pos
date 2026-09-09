6066bd630ac8fd11da34aeaa8f0ef646a9d762be
E-Z <68935117+E-Mrtnz@users.noreply.github.com>
2026-09-08 20:29:34 -0600

    refactor: keep main as application entry point

--- lib/main.dart
+++ lib/main.dart
@@ -1,59 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
-import 'package:stellar_pos/core/constants/app_constants.dart';
-import 'package:stellar_pos/core/providers/catalog_provider.dart';
-import 'package:stellar_pos/core/providers/debt_provider.dart';
-import 'package:stellar_pos/core/providers/electronic_balance_provider.dart';
-import 'package:stellar_pos/core/providers/printer_provider.dart';
-import 'package:stellar_pos/core/providers/product_provider.dart';
-import 'package:stellar_pos/core/providers/providers_provider.dart';
-import 'package:stellar_pos/core/providers/purchases_provider.dart';
-import 'package:stellar_pos/core/providers/sales_provider.dart';
-import 'package:stellar_pos/presentation/dashboard/main_dashboard_layout.dart';
+import 'package:stellar_pos/app.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-
   runApp(
-    MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => ProductProvider()),
-        ChangeNotifierProvider(create: (_) => CatalogProvider()),
-        ChangeNotifierProvider(create: (_) => ProvidersProvider()),
-        ChangeNotifierProvider(create: (_) => PurchasesProvider()),
-        ChangeNotifierProvider(create: (_) => ElectronicBalanceProvider()),
-        ChangeNotifierProvider(create: (_) => PrinterProvider()),
-        ChangeNotifierProvider(create: (_) => SalesProvider()),
-        ChangeNotifierProvider(
-          create: (context) => DebtProvider(context.read<SalesProvider>()),
-        ),
-      ],
-      child: const StellarPosApp(),
+    const AppProviders(
+      child: StellarPosApp(),
     ),
   );
-}
-
-class StellarPosApp extends StatelessWidget {
-  const StellarPosApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: AppStrings.appName,
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        fontFamily: 'SF Pro Display',
-        scaffoldBackgroundColor: AppColors.background,
-        primaryColor: AppColors.primary,
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: AppColors.primary,
-          primary: AppColors.primary,
-          surface: AppColors.background,
-        ),
-        useMaterial3: true,
-      ),
-      home: const MainDashboardLayout(),
-    );
-  }
 }
