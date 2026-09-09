fd9d549dbad9c9f763440f4e22e81e318294bf39
E-Z <68935117+E-Mrtnz@users.noreply.github.com>
2026-09-09 00:22:08 -0600

    feat(storage): wire sales and electronic balance repositories

--- lib/core/app/app_providers.dart
+++ lib/core/app/app_providers.dart
@@ -3,58 +3,45 @@ import 'package:provider/provider.dart';
 
 import 'package:stellar_pos/core/data/repositories/client_repository.dart';
 import 'package:stellar_pos/core/data/repositories/debt_movement_repository.dart';
+import 'package:stellar_pos/core/data/repositories/electronic_balance_account_repository.dart';
+import 'package:stellar_pos/core/data/repositories/electronic_balance_transaction_repository.dart';
 import 'package:stellar_pos/core/data/repositories/product_repository.dart';
 import 'package:stellar_pos/core/data/repositories/purchase_repository.dart';
+import 'package:stellar_pos/core/data/repositories/sale_repository.dart';
 import 'package:stellar_pos/core/providers/catalog_provider.dart';
+import 'package:stellar_pos/core/providers/electronic_balance_provider.dart';
 import 'package:stellar_pos/core/providers/printer_provider.dart';
 import 'package:stellar_pos/core/providers/product_provider.dart';
 import 'package:stellar_pos/features/catalog/catalog.dart';
 import 'package:stellar_pos/features/debts/debts.dart';
-import 'package:stellar_pos/features/electronic_balance/electronic_balance.dart';
 import 'package:stellar_pos/features/purchases/purchases.dart';
 import 'package:stellar_pos/features/sales/sales.dart';
 
-/// Composition root for presentation/application state dependencies.
 class AppProviders extends StatelessWidget {
   final Widget child;
-
   const AppProviders({required this.child, super.key});
 
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-        ChangeNotifierProvider(
-          create: (_) => CatalogProvider(
-            clientRepository: ClientRepository(),
-          )..loadClients(),
-        ),
+        ChangeNotifierProvider(create: (_) => CatalogProvider(clientRepository: ClientRepository())..loadClients()),
         ChangeNotifierProxyProvider<CatalogProvider, ProductProvider>(
-          create: (context) => ProductProvider(
-            catalogRegistrar: context.read<CatalogProvider>(),
-            repository: ProductRepository(),
-          )..load(),
-          update: (_, catalog, products) =>
-              products ??
-              ProductProvider(
-                catalogRegistrar: catalog,
-                repository: ProductRepository(),
-              )..load(),
+          create: (context) => ProductProvider(catalogRegistrar: context.read<CatalogProvider>(), repository: ProductRepository())..load(),
+          update: (_, catalog, products) => products ?? ProductProvider(catalogRegistrar: catalog, repository: ProductRepository())..load(),
         ),
         ChangeNotifierProvider(create: (_) => ProvidersProvider()),
+        ChangeNotifierProvider(create: (_) => PurchasesProvider(repository: PurchaseRepository())..load()),
         ChangeNotifierProvider(
-          create: (_) => PurchasesProvider(
-            repository: PurchaseRepository(),
+          create: (_) => ElectronicBalanceProvider(
+            accountRepository: ElectronicBalanceAccountRepository(),
+            transactionRepository: ElectronicBalanceTransactionRepository(),
           )..load(),
         ),
-        ChangeNotifierProvider(create: (_) => ElectronicBalanceProvider()),
         ChangeNotifierProvider(create: (_) => PrinterProvider()),
-        ChangeNotifierProvider(create: (_) => SalesProvider()),
+        ChangeNotifierProvider(create: (_) => SalesProvider(repository: SaleRepository())..load()),
         ChangeNotifierProvider(
-          create: (context) => DebtProvider(
-            context.read<SalesProvider>(),
-            movementRepository: DebtMovementRepository(),
-          )..load(),
+          create: (context) => DebtProvider(context.read<SalesProvider>(), movementRepository: DebtMovementRepository())..load(),
         ),
       ],
       child: child,
