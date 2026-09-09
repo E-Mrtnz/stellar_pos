3536f99d5ae104c2b70267d42afb4cab4bd4c74b
E-Z <68935117+E-Mrtnz@users.noreply.github.com>
2026-09-08 21:36:31 -0600

    refactor: remove product provider static catalog coupling

--- lib/core/providers/product_provider.dart
+++ lib/core/providers/product_provider.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/foundation.dart';
 
+import 'package:stellar_pos/core/domain/catalog/catalog_registrar.dart';
 import 'package:stellar_pos/core/models/product.dart';
-import 'package:stellar_pos/core/providers/catalog_provider.dart';
 import 'package:stellar_pos/core/utils/id_generator.dart';
 
 class ProductProvider extends ChangeNotifier {
   final List<Product> _products = [];
+  final CatalogRegistrar? _catalogRegistrar;
+
+  ProductProvider({CatalogRegistrar? catalogRegistrar}) : _catalogRegistrar = catalogRegistrar;
 
   List<Product> get products => List.unmodifiable(_products);
   List<Map<String, dynamic>> get productMaps => _products.map((p) => p.toMap()).toList(growable: false);
@@ -21,7 +24,7 @@ class ProductProvider extends ChangeNotifier {
   void addProduct(Product product) {
     final id = product.id.isEmpty ? IdGenerator.newId() : product.id;
     final normalized = product.copyWith(id: id, touchMetadata: false);
-    CatalogProvider.registerBrand(normalized.brand);
+    _catalogRegistrar?.registerBrandValue(normalized.brand);
     _products.add(normalized);
     notifyListeners();
   }
@@ -29,7 +32,7 @@ class ProductProvider extends ChangeNotifier {
   bool updateProduct(Product product) {
     final index = _products.indexWhere((p) => p.id == product.id);
     if (index < 0) return false;
-    CatalogProvider.registerBrand(product.brand);
+    _catalogRegistrar?.registerBrandValue(product.brand);
     _products[index] = product;
     notifyListeners();
     return true;
