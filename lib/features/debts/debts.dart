e8b9300345bc392953163e1f13215a1cb1e90d33
E-Z <68935117+E-Mrtnz@users.noreply.github.com>
2026-09-11 20:02:31 -0600

    feat: expose client groups from debts feature

--- lib/features/debts/debts.dart
+++ lib/features/debts/debts.dart
@@ -1,3 +1,5 @@
 /// Public entry point for the debts feature.
+export 'package:stellar_pos/core/models/client_group.dart';
 export 'package:stellar_pos/core/models/debt.dart';
+export 'package:stellar_pos/core/providers/client_group_provider.dart';
 export 'package:stellar_pos/core/providers/debt_provider.dart';
