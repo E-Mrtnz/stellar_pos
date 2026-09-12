19b53212a9a58e68f63916345c4b13cde6d50f44
E-Z <68935117+E-Mrtnz@users.noreply.github.com>
2026-09-11 20:00:28 -0600

    feat: register client group provider

--- lib/core/app/app_providers.dart
+++ lib/core/app/app_providers.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import 'package:stellar_pos/core/data/repositories/client_group_repository.dart';
 import 'package:stellar_pos/core/data/repositories/client_repository.dart';
 import 'package:stellar_pos/core/data/repositories/debt_movement_repository.dart';
 import 'package:stellar_pos/core/data/repositories/electronic_balance_account_repository.dart';
@@ -11,6 +12,7 @@ import 'package:stellar_pos/core/data/repositories/provider_route_repository.dar
 import 'package:stellar_pos/core/data/repositories/purchase_repository.dart';
 import 'package:stellar_pos/core/data/repositories/sale_repository.dart';
 import 'package:stellar_pos/core/providers/catalog_provider.dart';
+import 'package:stellar_pos/core/providers/client_group_provider.dart';
 import 'package:stellar_pos/core/providers/date_aware_sales_provider.dart';
 import 'package:stellar_pos/core/providers/electronic_balance_provider.dart';
 import 'package:stellar_pos/core/providers/printer_provider.dart';
@@ -83,6 +85,9 @@ class AppProviders extends StatelessWidget {
             context.read<SalesProvider>(),
             movementRepository: DebtMovementRepository(),
           )..load(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => ClientGroupProvider()..load(),
         ),
       ],
       child: child,
