4f60204d8bacea06bda16caf14c83f936d36b23a
E-Z <68935117+E-Mrtnz@users.noreply.github.com>
2026-09-26 03:43:35 -0600

    dev(storage): print Hive CE database directory

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
 
 import 'package:stellar_pos/app.dart';
 import 'package:stellar_pos/core/data/storage/local_storage.dart';
@@ -7,6 +8,8 @@ import 'package:stellar_pos/core/data/storage/storage_schema.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await LocalStorage.initialize();
+  final hiveDirectory = await getApplicationDocumentsDirectory();
+  debugPrint('HIVE CE DATABASE PATH: ${hiveDirectory.path}');
   await StorageSchema.initialize();
 
   runApp(
